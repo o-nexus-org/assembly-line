7167cfe43a51f056fafdb6fa0a01b7e95df26394
david-fortini <fortini.david@gmail.com>
2023-02-22 09:10:26 +0100

    added qr code

--- Makefile
+++ Makefile
@@ -5,9 +5,10 @@
 # drivers
 # https://www.brother.nl/ondersteuning/ql-700/downloadshttps://www.brother.nl/ondersteuning/ql-700/downloads
 
-# lsusb | grep Brother | cut -d ' ' -f6 | cut -d ':' -f1
-run_print:
-	brother_ql -p 'usb://0x04f9:0x2042' -b pyusb --model QL-700 print --label 29 --rotate 90 --threshold 0.5 ./test_QR.png
+# lsusb | grep Brother | cut -d ' ' -f6 | cut -d ':' -f1 
+# --rotate 90 --threshold 0.5 
+run_print:# 
+	brother_ql -p 'usb://0x04f9:0x2042' -b pyusb --model QL-700 print --label 29 zz_QR_mac.png
 
 run:
 	streamlit run app.py
--- src/qr.py
+++ src/qr.py
@@ -1,15 +1,106 @@
-import pyqrcode
-from PIL import Image
+""" 
+Code to generate PR code.
+"""
+from pathlib import Path
+import cv2
+import os
+import subprocess
+import qrcode
+from PIL import Image, ImageFont, ImageDraw, ImageOps
 
-url = pyqrcode.QRCode('https://afmelden.o-nexus.com/?gw=B8D61A188B2C',error = 'H')
-img_path = 'logo.png'
-url.png('test.png',scale=10)
-im = Image.open('test.png')
-im = im.convert("RGBA")
-logo = Image.open('logo.png')
-box = (135,135,235,235)
-im.crop(box)
-region = logo
-region = region.resize((box[2] - box[0], box[3] - box[1]))
-im.paste(region,box)
-im.show()
+
+def write_qr(mac) -> str:
+    QRcode = qrcode.QRCode(
+        error_correction=qrcode.constants.ERROR_CORRECT_H
+    )
+    url = f'https://afmelden.o-nexus.com/?mac={mac}'
+    QRcode.add_data(url)
+    QRcode.make()
+    # adding color to QR code
+    QRimg = QRcode.make_image(
+        fill_color='Black', back_color="white").convert('RGB')
+    # save the QR code generated
+    out_fp = f'temp_/QR_{mac}.png'
+    QRimg.save(out_fp)
+    return out_fp
+
+
+def add_mac_and_address_to_img(img_path: str, 
+                               mac: str,
+                               address: str,
+                               show:bool=False) -> str:
+    if len(address) > 20:
+        raise RuntimeError("Address too long, NOT SUPPORTED")
+
+    black_color_rgb = (0,0,0)
+    white_color_rgb = (255,255,255)
+    img = Image.open(img_path)
+    img = ImageOps.expand(img, border=19, fill=white_color_rgb)
+    draw = ImageDraw.Draw(img)
+    font_path = os.path.join(cv2.__path__[0],'qt','fonts','DejaVuSans.ttf')
+    font = ImageFont.truetype(font_path, size=52)
+    # on top of the QR code
+    draw.text((62,0),mac,(0,0,0),font=font,
+              align='center'
+            )
+    # bottom
+    draw.text((0,470),address,black_color_rgb,font=font,
+              align='center',
+              )
+    out_fp = f'temp_/QR_{mac}_print.png'
+    Path(out_fp).unlink(missing_ok=True)
+    img.save(out_fp)
+    if show:
+        img.show() 
+    print('QR code generated!')
+    return out_fp
+
+
+def create_qr(mac: str, 
+              address: str,
+              show: bool=False) -> str:
+    out_fp = write_qr(mac=mac)
+    out_fp = add_mac_and_address_to_img(img_path=out_fp, 
+                                        mac=mac,
+                                        address=address,
+                                        show=show)
+    return out_fp
+
+
+def run_bash(command: str) -> str:
+    process = subprocess.run(command, shell=True,
+                             check=True,
+                             capture_output=True)
+    out = process.stdout.decode('utf-8').strip()
+    return out
+
+
+def print_qr(img_fp: str, n_copy: int = 1):
+    first_part_usb_cmd = "lsusb | grep Brother | cut -d ' ' -f6 | cut -d ':' -f1 "
+    first_part_usb = run_bash(first_part_usb_cmd)
+    sec_part_usb_cmd = "lsusb | grep Brother | cut -d ' ' -f6 | cut -d ':' -f2 "
+    sec_part_usb = run_bash(sec_part_usb_cmd)
+    print(f"{first_part_usb} {sec_part_usb}")
+    cmd = f"brother_ql -p 'usb://0x{first_part_usb}:0x{sec_part_usb}' -b pyusb --model QL-700 print --label 29 {img_fp}"
+    while n_copy > 0:
+        out =  run_bash(cmd)
+        n_copy -= 1
+        print(out)
+    
+
+def create_and_print_qr(mac: str, 
+                        address: str,
+                        n_copy: int = 1) -> str:
+    out_fp = create_qr(mac=mac, address=address, show=False)
+    print_qr(img_fp=out_fp, n_copy=n_copy)
+    return out_fp
+
+
+if __name__ == "__main__":
+
+    mac = "B8D61A188B2C"
+    address = "Govert Flinkstraat 7"
+    out_fp = create_qr(mac=mac, address=address, show=True)
+    print_qr(img_fp=out_fp)
+
+
